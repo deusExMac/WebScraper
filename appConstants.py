d2ebdf3aae6015f9424079d81482b9db0642947b
deusExMac <39963356+deusExMac@users.noreply.github.com>
2022-10-20 22:11:01 +0300

    Updated comments in .exr files

--- appConstants.py
+++ appConstants.py
@@ -15,8 +15,8 @@ Version: 20/04/2022
 
 
 
-APPVERSION = '0.3.1a'
-VERSIONRELEASEDATE = '04/10/2022'
+APPVERSION = '0.3.6a'
+VERSIONRELEASEDATE = '20/10/2022'
 AUTHOR = 'mmt (tzagara@upatras.gr)'
 
 
